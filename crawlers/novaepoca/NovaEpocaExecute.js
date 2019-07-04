72620b92fd196baaccf6bf910360f8ecafdf0620
Luiz Felipe da Silva <luizfelipe.dasilva94@gmail.com>
2019-07-04 00:20:28 -0300

    logs

--- crawlers/novaepoca/NovaEpocaExecute.js
+++ crawlers/novaepoca/NovaEpocaExecute.js
@@ -5,8 +5,17 @@ const Queue             = require('../../rabbitmq/Queue');
 const argv              = require('yargs').argv;
 const figlet            = require('figlet');
 const NEUtils           = require('./NovaEpocaUtils');
+const Log               = require('../../models/log');
+const delay             = require('../../helper/delay');
 
-const PrepareEnvironment = () => {
+global.log = {};
+
+const PrepareEnvironment = async() => {
+
+    log.dtInicio = new Date();
+    log.funcao = PrepareEnvironment.name;
+    await SalvarLog(log);
+
     const db = require('../../lib/mongo').db;
     db.once('open', async function(){
         await NEUtils.saveBeighborhood();
@@ -16,61 +25,69 @@ const PrepareEnvironment = () => {
 }
 
 const ExecuteMainInfo = async () => {
+
+    log.dtInicio = new Date();
+    log.exec = ExecuteMainInfo.name;
+    await SalvarLog(log);
+
     // salva a capa dods imoveis.
     const db = require('../../lib/mongo').db;
     db.once('open', async function(){
-
         // nao passar parametros para crawlear o site todo.
         // "Meier", "prontos"
         const params = await BuildParams.GetParams("Cascadura", "prontos");
-
         for(let i = 0; i < params.length; i++){
-
             console.log(`${i+1} - PURPOSE.: ${params[i].purpose} - LOCATION.: ${params[i].location} - TYPE.: ${params[i].type}.`);
-            
             let params2 = {
                 location: params[i].location,
                 purpose: params[i].purpose,
                 type: params[i].type
             };
-            
             const NE = new NovaEpoca(params2);
-            
             let ok = await NE.getMainInfoPropeties();
-            
             if(ok){
                 console.log("Page screpped successfully!");
             }
-
         }
-        
+        const _log = await SalvarLog(log);
+        console.log(_log);
         db.close();
         process.exit(0);
     });
 }
 
 const ExecutePropertyUrl = async (frequency) => {
+
+    log.dtInicio = new Date();
+    log.funcao = ExecutePropertyUrl.name;
+    await SalvarLog(log);
+
     // pegar os imoveis que ainda nao foram crawleados e ativos.
     // se for por periodicidade, enviar pra fila independente de qualquer coisa e atualizar os dados.
     // enviar pra fila pra ser crawleado.
-
     const db = require('../../lib/mongo').db;
-
     db.once('open', async function(){
-        
         const result = await GenericExtractor.getDataToExtract();
-        
         for (let i = 0; i < result.length; i++) {
-            
             await Queue.sendToQueue(result[i], "NovaEpocaCrawler");
-
         }
-
         db.close();
         process.exit(0);
-
     });
     
+}
+
+const SalvarLog = async (log) => {
+    const db = require('../../lib/mongo').db;
+    db.once('open', async function(){
+        const LogData = new Log(log);
+        const result = await LogData.save()
+        .then(log => log)
+        .catch(err => console.error(err));
+        await delay(500);
+        // db.close();
+        // return result;
+    });
 }
 
 (async () => {
--- models/log.js
+++ models/log.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
 const LogSchema = new mongoose.Schema({
-
+    dtInicio: { type: Date, required: true },
+    exec: { type: String, required: true }
 });
 
 const Log = mongoose.model('Log', LogSchema);
